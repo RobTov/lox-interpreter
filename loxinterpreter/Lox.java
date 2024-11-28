aa683eb2b2b903a9abd21266136c7e1f2ad91ba5
RobTov <tovelrob25@gmail.com>
2024-11-28 01:23:05 -0500

    added error handling

--- loxinterpreter/Lox.java
+++ loxinterpreter/Lox.java
@@ -6,10 +6,11 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox[script]");
@@ -23,6 +24,9 @@ public class Lox {
     }
 
     private static void runFile(String path) throws IOException {
+        if (hadError)
+            System.exit(65);
+
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
     }
@@ -37,6 +41,7 @@ public class Lox {
             if (line == null)
                 break;
             run(line);
+            hadError = false;
         }
     }
 
@@ -48,5 +53,14 @@ public class Lox {
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.printf("[line %d] Error %s where : %s\n", line, where, message);
+        hadError = true;
     }
 }
