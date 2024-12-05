adc15f007a4694806e0a2e9e2be2fe25798ce1a0
RobTov <tovelrob25@gmail.com>
2024-12-05 16:13:41 -0500

    added runtime errors

---
+++ loxinterpreter/RuntimeError.java
@@ -0,0 +1,10 @@
+package loxinterpreter;
+
+public class RuntimeError extends RuntimeException {
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
