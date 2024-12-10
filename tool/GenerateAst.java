8dc1ba81a2dcaf1d23a41c0ca7b8e284a688ba98
RobTov <tovelrob25@gmail.com>
2024-12-10 16:39:55 -0500

    added the logic to assign variables

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -15,10 +15,11 @@ public class GenerateAst {
         String outputDir = args[0];
 
         defineAst(outputDir, "Expr", Arrays.asList(
-                "Binary: Expr left, Token operator, Expr right",
+                "Assign : Token name, Expr value",
+                "Binary : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
-                "Literal: Object value",
-                "Unary: Token operator, Expr right",
+                "Literal : Object value",
+                "Unary : Token operator, Expr right",
                 "Variable : Token name"));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
