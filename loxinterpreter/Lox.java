5fbfb6cd2550cf0a079763e02d190512d57ece0d
RobTov <tovelrob25@gmail.com>
2024-12-20 12:59:55 -0500

    implemented Resolver & handled errors

--- loxinterpreter/Lox.java
+++ loxinterpreter/Lox.java
@@ -68,7 +68,14 @@ public class Lox {
         if (hadError)
             return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
         // System.out.println(new AstPrinter().print(expression));
+        // * Stop if there was a resolution error.
+        if (hadError)
+            return;
+
         interpreter.interpret(statements);
     }
 
