4141accda44fc8f16ec2fa76705cb40e53dd0aab
RobTov <tovelrob25@gmail.com>
2024-12-21 18:12:51 -0500

    implemented  basic support to classes

---
+++ loxinterpreter/LoxClass.java
@@ -0,0 +1,14 @@
+package loxinterpreter;
+
+public class LoxClass {
+    final String name;
+
+    LoxClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
