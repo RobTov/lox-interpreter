e0182799677d7161ffa24ea68e90f5000c98adc3
RobTov <tovelrob25@gmail.com>
2024-12-24 09:34:11 -0500

    added the basics of inheritance

--- loxinterpreter/LoxClass.java
+++ loxinterpreter/LoxClass.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 public class LoxClass implements LoxCallable {
-    final String name;
+    private final String name;
+    private final LoxClass superclass;
     private final Map<String, LoxFunction> methods;
 
-    LoxClass(String name, Map<String, LoxFunction> methods) {
+    LoxClass(String name, LoxClass superclass, Map<String, LoxFunction> methods) {
         this.name = name;
+        this.superclass = superclass;
         this.methods = methods;
     }
 
